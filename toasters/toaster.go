909a108d9e4ba03d9288a6484048c5a438275198
nderimx <nderimx@gmail.com>
2019-01-04 22:48:52 +0100

    further clarifications on string representations

--- toasters/toaster.go
+++ toasters/toaster.go
@@ -3,26 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	s := "!@#$%^&*()_+/~}|{:'\\[]Ç"
-	s = s + string(byte(199)) + string(byte(169)) + "⺀"
-	fmt.Printf("String: %v\n %v \n", len(s), s)
+	// the rune 65533 maps to 'unknown character'
+	// when a byte value is utf-8 invalid it gets represented as the rune value 65533
+	s := "\n\xbd½\xb2\x3d\xbc\x20\xe2\x8c\x98" //+"Ç"
+	// s = s + string(byte(199)) + string(byte(169)) + "⺀"
+	// plain old string printing
+	fmt.Printf("\nString: %v\n %v \n\n", len(s), s)
 	// if ascii is exceeded, utf-8 encoding kicks in
 	// ( one char gets represented by more that one byte )
-	fmt.Printf("Bytes: %v\n %v \n", len([]byte(s)), []byte(s))
-	fmt.Println("Chars one by one")
+	fmt.Printf("Bytes: %v\n %v \n\n", len([]byte(s)), []byte(s))
 	var strs []rune
 	for _, c := range s {
 		// unicode representation without U+ prefix,
 		// more specifically utf-32, since a rune is an int32
 		strs = append(strs, c)
 	}
-	fmt.Printf("Bytes: %v\n %v \n", len(strs), strs)
+	fmt.Printf("Runes one by one: %v\n %v \n\n", len(strs), strs)
 
-	fmt.Println("Bytes one by one")
 	var bts []byte
 	for _, c := range s {
-		// prints only the least significant byte if it exeeds bytes
+		// prints only the least significant utf-32 byte if it exeeds 256 bits
 		bts = append(bts, byte(c))
 	}
-	fmt.Printf("Bytes: %v\n %v \n", len(bts), bts)
+	fmt.Printf("Bytes one by one: %v\n %v \n\n", len(bts), bts)
+
+	fmt.Printf("Hex bytes: % x\n\n", s)
+
+	fmt.Printf("Hex bytes w/ escaped unprintables: %q\n\n", s)
+
+	fmt.Printf("Hex bytes w/ escaped non ascii: %+q\n\n", s)
 }
